8c41fefe9ebd1774b4633c130859fc3e8b865ba6
Usmanov <roma.maksato@gmail.com>
2023-04-08 17:13:27 +0600

    new

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import airplane from './assets/images/5776896.png'
 import visa from './assets/images/visa.png'
 import whatsapp from './assets/images/whatsapp-svgrepo-com (1).svg'
 import instagram from './assets/images/instagram.png'
+import location from './assets/images/location.png'
 
 function App() {
   return (
@@ -14,25 +15,27 @@ function App() {
           <div className="title"><i className='bx bx-timer'></i><h5> Горящие туры </h5></div>
           <div className="title title-img"><img src={airplane} alt="airplane"/><h5> Авиабилеты </h5></div>
           <div className="title title-img"><img src={visa} alt="airplane"/><h5> Визы </h5></div>
-          <div>🏠Адреса и 📲контакты г.Бишкек</div>
         </div>
         <div className="container-text">
         </div>
-        {/*<section className="container">*/}
-        {/*  <div className="container_enlaces">*/}
-        {/*    <a href="https://wa.me/+996709750585" className="enlaces w-full whatsapp"><p>Написать менеджеру по авиабилетам (Сауле)</p><i className='bx bxl-whatsapp'></i></a>*/}
-        {/*    <a href="https://instagram.com/beeflykg?igshid=YmMyMTA2M2Y=" className="enlaces w-full instagram"><p>Instagram</p><i className='bx bxl-instagram'></i></a>*/}
-        {/*  </div>*/}
-        {/*  <div className="container_enlaces">*/}
-        {/*    <a href="#" className="enlaces w-full whatsapp"><p>WhatsApp</p><i className='bx bxl-whatsapp'></i></a>*/}
-        {/*  </div>*/}
-        {/*</section>*/}
         <div className="links">
           <ul>
             <li className="itemsCenter itemsInBox itemColor w-full">
               <img src={whatsapp} alt="" width={30}/>
               <a href="https://wa.me/+996709750585" className="link"> WhatsApp менеджеру по авиабилетам
-                <p>Сауле</p>
+                <p><b>Сауле</b></p>
+              </a>
+            </li>
+            <li className="itemsCenter itemsInBox itemColor w-full">
+              <img src={whatsapp} alt="" width={30}/>
+              <a href="https://wa.me/+996502750585" className="link"> WhatsApp менеджера по турам
+                <p><b>Бексултан</b></p>
+              </a>
+            </li>
+            <li className="itemsCenter itemsInBox itemColor w-full">
+              <img src={whatsapp} alt="" width={30}/>
+              <a href="https://chat.whatsapp.com/CNZrHJbUX5O1TOuPYcrucu" className="link">Вступить в WhatsApp группу
+                горящих <u>туров</u>
               </a>
             </li>
             <li className="itemsCenter itemsInBox itemColor w-full instagram">
@@ -41,17 +44,24 @@ function App() {
                 <p>Instagram</p>
               </a>
             </li>
-
-            {/*<li><a href="https://go.2gis.com/26o5mz"*/}
-            {/*       className="link">Орто-Сай (Адресс на карте)*/}
-            {/*  <span><br/>Все для салонов красоты</span>*/}
-            {/*</a>*/}
-            {/*</li>*/}
+            <li className="itemsCenter itemsInBox itemColor w-full instagram">
+              <img src={location} alt="" width={30}/>
+              <a href="https://2gis.kg/bishkek/geo/70000001067955342" className="link">
+                <p>Чуй/Советская (Адресс на карте)</p>
+              </a>
+            </li>
+            <li className="itemsCenter itemsInBox itemColor w-full instagram">
+              <img src={location} alt="" width={30}/>
+              <a href="https://2gis.kg/bishkek/geo/70000001067955342" className="link">
+                <p> Ахунбаева/Алматинская (Адресс на карте)</p>
+              </a>
+            </li>
           </ul>
         </div>
       </div>
       <div className="footer">
-        @copyright
+        <h1>Работаем круглосуточно 24/7</h1>
+        {/*<div>🏠Адреса и 📲контакты г.Бишкек</div>*/}
       </div>
     </div>
 
